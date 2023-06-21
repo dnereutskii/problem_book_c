3e329306e6722e6453861c389b959ba38486b0df
dmitriinereutskii <dmitriy.93.23@gmail.com>
2023-06-21 11:04:28 +0300

    [5.03.1] add std names for two files

--- 5_os_objects_and_services/5_03_1_three_files/main.c
+++ 5_os_objects_and_services/5_03_1_three_files/main.c
@@ -18,16 +18,17 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define BLOCK_SIZE 1024
-
 struct stream_poll {
     unsigned int cnt; /* counter of line characters */
     bool space;       /* space flag */
 };
 
+static const char file_text_wr_std_name[] = "text_mod.txt"; 
+static const char file_bin_std_name[] = "bin_cnt"; 
+
 int main(int argc, char **argv)
 {  
-    if (argc < 4) {
+    if (argc < 2) {
         fprintf(stderr, "too few arguments\n");
         return 1;
     }
@@ -37,26 +38,42 @@ int main(int argc, char **argv)
         .space = false
     };
     int c;
+    const char *file_name;
 
-    FILE *text_read = fopen(argv[1], "r");
-    if (text_read == NULL) {
+    /* first file open */
+    FILE *file_text_rd = fopen(argv[1], "r");
+    if (file_text_rd == NULL) {
         perror(argv[1]);
         exit(1);
     }
 
-    FILE *text_write = fopen(argv[2], "w"); /* truncate or create for writing */
-    if (text_write == NULL) {
+    /* second file open */
+    if (argc < 3)
+        file_name = file_text_wr_std_name;
+    else
+        file_name = argv[2];
+    FILE *file_text_wr = fopen(file_name, "w"); /* truncate or create for writing */
+    if (file_text_wr == NULL) {
         perror(argv[2]);
+        fclose(file_text_rd);
         exit(2);
     }
-
-    int binary_write = open(argv[3], O_WRONLY|O_CREAT|O_TRUNC, 0666);
-    if (binary_write == -1) {
-	perror(argv[3]);
-	exit(3);
+ 
+    /* third file open */
+    if (argc < 4)
+        file_name = file_bin_std_name;
+    else
+        file_name = argv[3];
+    int file_bin = open(file_name, O_WRONLY|O_CREAT|O_TRUNC, 0666);
+    if (file_bin == -1) {
+	    perror(argv[3]);
+        fclose(file_text_rd);
+        fclose(file_text_wr);
+	    exit(3);
     }
 
-    while ((c = fgetc(text_read)) != EOF) {
+    /* chars handler */
+    while ((c = fgetc(file_text_rd)) != EOF) {
         sp.cnt++;
         if (c == ' ' && sp.cnt == 1) /* new line */
             sp.space = true;
@@ -67,17 +84,16 @@ int main(int argc, char **argv)
             write(file_bin, &sp.cnt, sizeof(sp.cnt)); 
             sp.cnt = 0;
             sp.space = false;
-
             continue;
         } 
 
         if (sp.space)
-            fputc(c, text_write);
+            fputc(c, file_text_wr);
     }
 
-    fclose(text_read);
-    fclose(text_write);
-    close(binary_write);
+    fclose(file_text_rd);
+    fclose(file_text_wr);
+    close(file_bin);
 
     return 0;
 }
