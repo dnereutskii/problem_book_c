b192d4bf6cb83ec4d013e20cd81f6cc8b3d933f3
dnereutskii <dmitriy.93.23@gmail.com>
2023-07-07 07:58:38 +0300

    fix define

--- 5_os_objects_and_services/5_03_2_analyze_bin/bingen/main.c
+++ 5_os_objects_and_services/5_03_2_analyze_bin/bingen/main.c
@@ -16,7 +16,7 @@
 #include <stdio.h>
 // #include <sys/types.h>
 // #include <sys/stat.h>
-// #include <unistd.h>
+#include <unistd.h>
 #include <fcntl.h>
 // #include <errno.h>
 #include <stdlib.h> /* for exit() */
