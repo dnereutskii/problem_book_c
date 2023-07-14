a7a646f6ae673e19524abcf746ed071d679013fa
dmitriinereutskii <dmitriy.93.23@gmail.com>
2023-07-14 18:16:09 +0300

    [5.04] mod main

--- 5_os_objects_and_services/5_04_lines_counter/main.c
+++ 5_os_objects_and_services/5_04_lines_counter/main.c
@@ -15,24 +15,60 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define READ_BLOCK_SIZE 4096
-#define MIN_ARGS_CNT    2
+#define READ_BLOCK_SIZE     4096
+#define MIN_ARGS_CNT        2
+#define SYSCALL_OPEN_ERR    (-1)
+#define SYSCALL_READ_EOF    (0)
+#define SYSCALL_READ_ERR    (-1)
+
+static size_t count_lines(int fd);
+static size_t count_char(char *buf, size_t buf_size, char ch);
 
 int main(int argc, char **argv)
 {
     if (argc < MIN_ARGS_CNT) {
-	fprintf(stderr, "too few arguments\n");
-	return 1;
+        fprintf(stderr, "too few arguments\n");
+        return 1;
     }
 
     char *text_file_name = argv[1];
-    int text_file = open(text_file_name, O_RDONLY|O_APPEND);
-    if (text_file == 0) {
-	perror(text_file_name);
-	return 1;
+    int text_file = open(text_file_name, O_RDONLY);
+    if (text_file == SYSCALL_OPEN_ERR) {
+        perror(text_file_name);
+        return 1;
     }
+
+    size_t line_cnt = count_lines(text_file);
+    printf("%u\n", line_cnt);
 
     close(text_file);
     
     return 0;
+}
+
+static size_t count_lines(int fd)
+{
+    ssize_t rd;
+    size_t line_cnt = 0;
+    char buf[READ_BLOCK_SIZE];
+
+    while((rd = read(fd, buf, READ_BLOCK_SIZE)) != SYSCALL_READ_EOF) {
+        line_cnt += count_char(buf, rd, '\n');
+    }
+    
+    return line_cnt;
+}
+
+static size_t count_char(char *buf, size_t buf_size, char ch)
+{
+    size_t char_cnt = 0;
+
+    while (buf_size) {
+        if (*buf == ch)
+            char_cnt++;
+        buf++;
+        buf_size--;
+    }
+
+    return char_cnt;
 }
