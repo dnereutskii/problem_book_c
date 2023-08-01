dc8a069e7f0c16cd321a984ace0ffc91495576a2
dnereutskii <dmitriy.93.23@gmail.com>
2023-08-01 08:21:16 +0300

    [5.07.1] add structs and enums

--- 5_os_objects_and_services/5_07_1_table/main.c
+++ 5_os_objects_and_services/5_07_1_table/main.c
@@ -24,7 +24,7 @@
 
 /* USER defines */
 #define RDWR_BLOCK_SIZE     4096
-#define MIN_ARGS_CNT        4
+#define MIN_ARGS_CNT        3
 #define CNT_SIZE_BYTES      4
 #define STRING_MAX_LEN      59
 #define STRING_RECORD_LEN   (STRING_MAX_LEN + 1)
@@ -33,17 +33,37 @@
 
 
 enum {
-    ARGS_INDX_FILE_NAME = 1,
+    ARGS_INDX_PROG_NAME,
+    ARGS_INDX_FILE_NAME,
     ARGS_INDX_CMD,
     ARGS_INDX_ID,
+
+    ARGS_INDX_NUM,
+};
+
+enum {
+    CMD_INDX_ADD,
+    CMD_INDX_QUARY,
+    CMD_INDX_LIST,
+
+    CMD_INDX_NUM
 };
 
 struct args_info {
     int fd;
-    unsigned key; /* 32-bit unsigned integer */
     const char *file_name;
+    const char *cmd;
+    int cmd_id;
+    const char *item;
 };
 
+const char * cmd_strings[] = {
+    [CMD_INDX_ADD] = "add",
+    [CMD_INDX_QUARY] = "quary",
+    [CMD_INDX_LIST] = "list",
+};
+
+static void fill_args_info(struct args_info ai);
 
 int main(int argc, char **argv)
 {
@@ -56,17 +76,17 @@ int main(int argc, char **argv)
     
     /* open file (text or binary)*/
     ainfo.file_name = argv[ARGS_INDX_FILE_NAME];
-    ainfo.fd = open(ainfo.file_name, O_RDWR);
+    ainfo.fd = open(ainfo.file_name, O_RDWR|O_CREAT);
     if (ainfo.fd == SYSCALL_OPEN_ERR) {
         perror(ainfo.file_name);
         return 1;
     }
 
     /* get key */
-    if (sscanf(argv[ARGS_INDX_KEY], "%u", &ainfo.key) != 1) {
+    /*    if (sscanf(argv[ARGS_INDX_KEY], "%u", &ainfo.key) != 1) {
         fprintf(stderr, "error handling %s\n", argv[ARGS_INDX_KEY]);
         return 1;
-    }
+        }*/
 
 
 
