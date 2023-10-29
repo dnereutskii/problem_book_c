1716cc17c66138ad445559797654b605dca38a53
dnereutskii <dmitriy.93.23@gmail.com>
2023-10-29 22:35:45 +0300

    [5.12] add program skeleton

---
+++ 5_os_objects_and_services/5_12_exec/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    int pid, status;
+
+    if (argc < 2) {
+        fprintf(stderr, "too few arguments\n");
+        return 1;
+    }
+    pid = fork();
+    if (pid == -1) { /* make process error */
+        perror("fork failure\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (pid == 0) { /* child process */   
+        //execlp("ls", "ls", "-la", "/var", NULL);
+        if (execv(argv[1], &argv[1]) == -1) {
+                perror(argv[1]); /* exec returns control -> error */
+                exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    /* parent process */
+    wait(&status); /* wait for finishing of child process */
+    printf("%d\n", status);
+        
+    return 0;
+}
